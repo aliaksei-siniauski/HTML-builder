f25ed26fdd5f48246f15ddece41f50269bea46cd
github-username <mail@gmail.com>
2022-11-06 17:33:05 +0100

    feat: add solution to the forth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const promise = require("fs/promises");
+const path = require("path");
+const source = path.join(__dirname, "files");
+const destination = path.join(__dirname, "files-copy");
 
+async function copyDir() {
+  try {
+    const removingFolder = await promise.rm(
+      path.join(__dirname, "files-copy"),
+      {
+        recursive: true,
+        force: true,
+      }
+    );
+    const newFolder = await promise.mkdir(path.join(__dirname, "files-copy"), {
+      recursive: true,
+    });
+    const data = await promise.readdir(source);
+
+    data.forEach((item) => {
+      promise.copyFile(path.join(source, item), path.join(destination, item));
+    });
+  } catch (err) {
+    console.log("Something is wrong " + err);
+  }
+}
+copyDir();
