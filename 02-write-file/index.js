8d5bedd9cb675d6762e73e802b6bd26c48205d78
github-username <mail@gmail.com>
2022-11-06 14:22:39 +0100

    feat: add solution to the task 2

---
+++ 02-write-file/index.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const readline = require("readline");
+const path = require("path");
+const process = require("process");
+
+const pathToFile = path.join("02-write-file", "text.txt");
+const textFile = fs.createWriteStream(pathToFile);
+
+console.log("Hello, What's your name?");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", (text) => {
+  if (text === "exit") process.exit();
+
+  textFile.write(text + "\n");
+});
+
+rl.on("error", (err) => console.log(err));
+process.on("SIGINT", () => console.log("See you next time!"));
+process.on("exit", () => console.log("See you next time!"));
